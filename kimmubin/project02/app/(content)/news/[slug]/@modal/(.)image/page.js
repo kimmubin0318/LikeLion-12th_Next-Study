77674ca5830e12e356e056f9db86a41b71cd7d2d
Kim, mubin <kimmubin0318@gmail.com>
2024-09-22 14:09:53 +0900

    dfadfnjfgs

---
+++ kimmubin/project02/app/(content)/news/[slug]/@modal/(.)image/page.js
@@ -0,0 +1,25 @@
+import { notFound } from 'next/navigation';
+import ModalBackdrop from '@/components/modal-backdrop';
+import { getNewsItem } from '@/lib/news';
+
+export default async function InterceptedImagePage({ params }) {
+    const newsItemSlug = params.slug;
+    const newsItem = await getNewsItem(newsItemSlug);
+
+    if (!newsItem) {
+        notFound();
+    }
+    return (
+        <>
+            <ModalBackdrop />
+            <dialog className="modal">
+                <div className="fullscreen-image">
+                    <img
+                        src={`/images/news/${newsItem.image}`}
+                        alt={newsItem.title}
+                    />
+                </div>
+            </dialog>
+        </>
+    );
+}
